2a151c46de10ee42be555aaa2e768869b981e9ae
yehuda buzaglo <yehuda buzaglo>
2017-06-14 17:51:56 -0600

    added wall coliders and adjusted phisics engine

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+
+    private bool hasStarted = false;  //initialized to false because game hasent started
+
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position; // sets the location of the ball to be on paddle
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            //lock the ball relative to the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector; //sets location of ball on paddle every frame
+        }
+
+        //wait for a mouse click to launch
+        if (Input.GetMouseButtonDown(0)) //basiclly on click
+        {
+            print("mouse clicked");
+            hasStarted = true; // on click means that the game has started
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f); //new syntax for rigidbody give it an initial velocity with the vector 2 property
+        }
+	}
+}
